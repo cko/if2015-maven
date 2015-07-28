70bce59d01a239f7dbcba33c0fdaf5484f8a9864
Christine Koppelt <ch.ko123@gmail.com>
2015-07-28 18:59:41 +0200

    teil 1

---
+++ uebung-hallowelt/src/main/java/HalloWelt.java
@@ -0,0 +1,11 @@
+public class HalloWelt {
+
+    public static void main(String[] args) {
+	HalloWelt halloWelt = new HalloWelt();
+	System.out.println(halloWelt.getGreeting());
+    }
+
+    public String getGreeting() {
+	return "Hallo Welt";
+    }
+}
